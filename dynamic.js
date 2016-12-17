9c111263ef329ce8cc836a1c79c7816e0a4f3c26
Anirudh-thakur <anirudh.thakur94@gmail.com>
2016-12-17 11:46:52 +0530

    Add files via upload

---
+++ dynamic.js
@@ -0,0 +1,115 @@
+function opac()
+{
+ document.getElementById("header").style.opacity="0.7";
+ return true;
+}
+function opacrem()
+{
+ document.getElementById("header").style.opacity="1";
+ return true;
+}
+function changecolor()
+{
+ document.getElementById("h1").style.color="white";
+ return true;
+}
+function changecoloragain()
+{
+ document.getElementById("h1").style.color="black";
+ return true;
+}
+function backgroundchange()
+{
+ document.getElementById("nav").style.background="black";
+ return true;
+}
+function backgroundchangeagain()
+{
+ document.getElementById("nav").style.background="none";
+ return true;
+}
+function backgroundchange1()
+{
+ document.getElementById("dis").style.background="black";
+ return true;
+}
+function backgroundchangeagain1()
+{
+ document.getElementById("dis").style.background="none";
+ return true;
+}
+function aarithmos()
+{
+ document.getElementById("aarithmos").style.opacity="1";
+ return true;
+}
+function aarithmosagain()
+{
+ document.getElementById("aarithmos").style.opacity="0.5";
+ return true;
+}
+function blog()
+{
+ document.getElementById("blog").style.opacity="1";
+ return true;
+}
+function blogagain()
+{
+ document.getElementById("blog").style.opacity="0.5";
+ return true;
+}
+function trendylocations()
+{
+ document.getElementById("trendylocations").style.opacity="1";
+ return true;
+}
+function trendylocationsagain()
+{
+ document.getElementById("trendylocations").style.opacity="0.5";
+ return true;
+}
+function investin()
+{
+ document.getElementById("investin").style.opacity="1";
+ return true;
+}
+function investinagain()
+{
+ document.getElementById("investin").style.opacity="0.5";
+ return true;
+}
+function fb()
+{
+ document.getElementById("fb").style.opacity="1";
+ return true;
+}
+function fbagain()
+{
+ document.getElementById("fb").style.opacity="0.5";
+ return true;
+}
+function twitter()
+{
+ document.getElementById("twitter").style.opacity="1";
+ return true;
+}
+function twitteragain()
+{
+ document.getElementById("twitter").style.opacity="0.5";
+ return true;
+}
+function mail()
+{
+ document.getElementById("mail").style.opacity="1";
+ return true;
+}
+function mailagain()
+{
+ document.getElementById("mail").style.opacity="0.5";
+ return true;
+}
+function clearloader()
+{
+ document.getElementById("loader").style.opacity="0";
+ return true;
+}
